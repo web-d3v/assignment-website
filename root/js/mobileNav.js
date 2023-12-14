8fe16221aa832a8052b66dce5f9925da6a6328f3
Charlie <charlessbird+github@outlook.com>
2023-12-14 15:30:20 +0000

    fixed navbar and added page 1 content

--- root/js/mobileNav.js
+++ root/js/mobileNav.js
@@ -1,26 +1,47 @@
-const toggleButton = document.querySelector("#mobileNavToggle");
-const showElement = (element, transition, height) => {
-    element.style.height = window.screen.height
-    element.classList.toggle("show")
-    if (element.classList.contains("hidden")) {
-        element.classList.remove("hidden")
+const openNavButton = document.querySelector("#mobileNavOpen");
+const closeNavButton = document.querySelector("#mobileNavClose");
+const TIMEOUT = 1498;
+
+const MENU_ICON = ["fas", "fa-bars"];
+const XMARK_ICON = ["fa-solid", "fa-xmark"];
+
+const handleMenuIconChange = () => {
+    const icon = openNavButton.querySelector("i");
+    icon.style.transition = "all ease-in 1.3s";
+    if (openNavButton.classList.contains("open")) {
+        icon.style.transform = "rotate(360deg)"
+        setTimeout(() => {
+            icon.classList.remove(...XMARK_ICON)
+            icon.classList.add(...MENU_ICON)
+        }, TIMEOUT);
+        openNavButton.classList.remove("open")
+    } else {
+        icon.style.transition = "all ease-in 1.3s";
+        icon.style.transform = "rotate(270deg)"
+        setTimeout(() => {
+            icon.classList.add(...XMARK_ICON)
+            icon.classList.remove(...MENU_ICON)
+            icon.style.transform = "rotate(-90deg)";
+        }, TIMEOUT);
+        openNavButton.classList.add("open")
     }
 }
-toggleButton.addEventListener("click", (e) => {
-    const nav = document.querySelector("nav");
-    const navItems = nav.querySelector("section");
-    const dropDowns = navItems.querySelectorAll(".dropDown");
-    for (var i = 0; i < dropDowns.length; i++) {
-        const dropDown = dropDowns[i];
-        const dropDownContent = dropDown.querySelector(".dropDownContent");
-        showElement(dropDown);
-    }
-    showElement(navItems)
-    showElement(nav, true, navItems.offsetHeight)
-    const closeNav = nav.querySelector(".closeNav")
-    showElement(closeNav)
-    closeNav.style.marginTop = "3rem"
-    showElement(closeNav.querySelector("i"))
-    closeNav.querySelector("i").style.fontSize = "4rem"
+const nav = document.querySelector("nav");
+nav.style.transition = "height ease 0.55s";
+const header = document.querySelector("header");
+
+openNavButton.addEventListener("click", (e) => {
+
+    header.style.borderBottom = "2px solid white";
+    nav.style.height = "100vh";
+    openNavButton.style.borderTop = "2px groove var(--accent1)"
+    openNavButton.style.borderBottom = "2px groove var(--accent1)"
+})
+
+closeNavButton.addEventListener("click", (e) => {
     
+    openNavButton.style.borderTop = "none"
+    openNavButton.style.borderBottom = "none"
+    header.style.borderBottom = "none";
+    nav.style.height = "0";
 })
