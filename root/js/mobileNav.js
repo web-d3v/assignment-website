3339987489ca0e3c80ccd3f52e7bb1d14e2c3560
Charlie <charlessbird+github@outlook.com>
2023-12-19 15:09:08 +0000

    update

--- root/js/mobileNav.js
+++ root/js/mobileNav.js
@@ -1,47 +1,42 @@
 const openNavButton = document.querySelector("#mobileNavOpen");
 const closeNavButton = document.querySelector("#mobileNavClose");
-const TIMEOUT = 1498;
 
-const MENU_ICON = ["fas", "fa-bars"];
-const XMARK_ICON = ["fa-solid", "fa-xmark"];
-
-const handleMenuIconChange = () => {
-    const icon = openNavButton.querySelector("i");
-    icon.style.transition = "all ease-in 1.3s";
-    if (openNavButton.classList.contains("open")) {
-        icon.style.transform = "rotate(360deg)"
-        setTimeout(() => {
-            icon.classList.remove(...XMARK_ICON)
-            icon.classList.add(...MENU_ICON)
-        }, TIMEOUT);
-        openNavButton.classList.remove("open")
-    } else {
-        icon.style.transition = "all ease-in 1.3s";
-        icon.style.transform = "rotate(270deg)"
-        setTimeout(() => {
-            icon.classList.add(...XMARK_ICON)
-            icon.classList.remove(...MENU_ICON)
-            icon.style.transform = "rotate(-90deg)";
-        }, TIMEOUT);
-        openNavButton.classList.add("open")
-    }
-}
 const nav = document.querySelector("nav");
 nav.style.transition = "height ease 0.55s";
 const header = document.querySelector("header");
 
 openNavButton.addEventListener("click", (e) => {
-
     header.style.borderBottom = "2px solid white";
     nav.style.height = "100vh";
-    openNavButton.style.borderTop = "2px groove var(--accent1)"
-    openNavButton.style.borderBottom = "2px groove var(--accent1)"
+    openNavButton.classList.add("active");
 })
 
 closeNavButton.addEventListener("click", (e) => {
-    
-    openNavButton.style.borderTop = "none"
-    openNavButton.style.borderBottom = "none"
+    openNavButton.classList.remove("active");
     header.style.borderBottom = "none";
     nav.style.height = "0";
+})
+
+const toPX = (rem) => { return (rem * 16) }
+const toRem = (px) => { return (px / 16) }
+
+window.addEventListener("resize", () => {
+    const width = window.screen.availWidth;
+    if (width > toPX(67.5)) {
+        // put navbar back to desktop view
+        console.log("TRUE!!!")
+
+        nav.style.height = "3rem";
+        if (openNavButton.classList.contains("active")) {
+            openNavButton.classList.remove("active")
+        }
+    } else {
+        // put navbar to mobile view
+        console.log("FALSEE!!!!")
+        nav.style.height = "0";
+        header.style.borderBottom = "none";
+        if (openNavButton.classList.contains("active")) {
+            openNavButton.classList.remove("active")
+        }
+    }
 })
