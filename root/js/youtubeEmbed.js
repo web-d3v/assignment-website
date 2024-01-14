c07bab5ac82054248a70a3fb1ec350ec360c4b80
UntoldGam <93488596+UntoldGam@users.noreply.github.com>
2024-01-14 23:52:19 +0000

    finished FS Bike Page, made layout fixes

---
+++ root/js/youtubeEmbed.js
@@ -0,0 +1,54 @@
+function createIFrame(div) {
+    var iframe = document.createElement("iframe");
+    iframe.setAttribute(
+        "src",
+        "https://www.youtube.com/embed/" + div.getAttribute("data-id") + "?autoplay=1&rel=0"
+    );
+    iframe.setAttribute("frameborder", "0");
+    iframe.setAttribute("allowfullscreen", "1");
+    iframe.setAttribute(
+        "allow",
+        "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+    );
+    div.parentNode.replaceChild(iframe, div);
+}
+
+var playerElements = document.querySelectorAll(".youtube-player");
+playerElements.forEach(player => {
+    var videoId = player.getAttribute("data-id")
+    var div = document.createElement("div");
+    div.setAttribute("data-id", videoId);
+    var thumbNode = document.createElement("img");
+    thumbNode.src = "https://i.ytimg.com/vi/ID/hqdefault.jpg".replace(
+        "ID",
+        videoId
+    );
+    div.appendChild(thumbNode);
+    var playButton = document.createElement("div");
+    playButton.setAttribute("class", "play");
+    div.appendChild(playButton);
+    div.onclick = function () {
+        createIFrame(this);
+    };
+    player.appendChild(div);
+})
+/*
+for (var n = 0; n < playerElements.length; n++) {
+    var videoId = playerElements[n].dataset.id;
+    var div = document.createElement("div");
+    div.setAttribute("data-id", videoId);
+    var thumbNode = document.createElement("img");
+    thumbNode.src = "https://i.ytimg.com/vi/ID/hqdefault.jpg".replace(
+        "ID",
+        videoId
+    );
+    div.appendChild(thumbNode);
+    var playButton = document.createElement("div");
+    playButton.setAttribute("class", "play");
+    div.appendChild(playButton);
+    div.onclick = function () {
+        createIFrame(this);
+    };
+    playerElements[n].appendChild(div);
+} 
+*/
